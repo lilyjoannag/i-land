16878a99a63e40cc512ba042e2f7ba58ea8a6b7a
Lily Gasztowtt <lilyjoannag@gmail.com>
2018-11-27 11:16:44 +0100

    updates on island show and new

--- app/controllers/islands_controller.rb
+++ app/controllers/islands_controller.rb
@@ -3,6 +3,10 @@ class IslandsController < ApplicationController
     @islands = Island.all
   end
 
+  def new
+    @island = Island.new
+  end
+
   def show
     @island = Island.find(params[:id])
   end
