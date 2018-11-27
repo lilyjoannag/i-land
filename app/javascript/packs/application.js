54579b47074901f254d9da3865eeabc40093eb1b
Nicolas Descreux <nicolasdescreux@hotmail.fr>
2018-11-27 10:56:14 +0100

    add  carousel to index

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,18 @@
 import "bootstrap";
+// import {carousel} from "components/carousel"
+import "slick-carousel"
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const carousel = () => {
+  $(document).ready(function(){
+    $('.carousel').slick({
+      // infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+    });
+  });
+};
+
+carousel()
