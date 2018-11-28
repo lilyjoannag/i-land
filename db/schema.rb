a11c524a4956b2540a25429da0149e1b2f1151fd
Lily Gasztowtt <lilyjoannag@gmail.com>
2018-11-28 13:19:48 +0100

    white to cards in show and edit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_27_143312) do
+ActiveRecord::Schema.define(version: 2018_11_28_115947) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,8 +36,8 @@ ActiveRecord::Schema.define(version: 2018_11_27_143312) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.json "photos"
     t.string "address"
+    t.text "photos", default: [], array: true
     t.index ["user_id"], name: "index_islands_on_user_id"
   end
 
