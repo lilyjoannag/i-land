de0779a89b0423d45f65bc16203c7ddb82fb2d13
Lily Gasztowtt <lilyjoannag@gmail.com>
2018-11-30 13:15:59 +0100

    reviews part 1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_29_162742) do
+ActiveRecord::Schema.define(version: 2018_11_30_114524) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,14 @@ ActiveRecord::Schema.define(version: 2018_11_29_162742) do
     t.index ["user_id"], name: "index_islands_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.bigint "island_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["island_id"], name: "index_reviews_on_island_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -64,4 +72,5 @@ ActiveRecord::Schema.define(version: 2018_11_29_162742) do
   add_foreign_key "bookings", "islands"
   add_foreign_key "bookings", "users"
   add_foreign_key "islands", "users"
+  add_foreign_key "reviews", "islands"
 end
